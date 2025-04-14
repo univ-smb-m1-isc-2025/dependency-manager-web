c707921dd17fce730f86a772255227f1cc5a7873
Oziphos <chevalier.c.contact@gmail.com>
2025-04-14 05:38:43 +0200

    fix: add / route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { PublicGuard } from './guards/public.guard';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DepotListComponent } from './components/depot-list/depot-list.component';
 import { DepotDetailComponent } from './components/depot-detail/depot-detail.component';
 export const routes: Routes = [
+  { path: '', redirectTo: '/depots', pathMatch: 'full' },
   // Public routes
-  { path: '', component: HomeComponent, canActivate: [PublicGuard] },
   { path: 'login', component: LoginComponent, canActivate: [PublicGuard] },
   {
     path: 'register',
